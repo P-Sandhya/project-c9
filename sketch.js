e5435bfab6a42454c9e17c26305405a495c869d7
P-Sandhya <89297141+P-Sandhya@users.noreply.github.com>
2021-08-30 20:14:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+
+
+function setup() {
+
+  createCanvas(400,400)
+  
+  }
+  
+  function draw () {
+      background("GREY")                         
+  
+   
+  
+  if(keyIsDown(DOWN_ARROW))
+  
+  background("RED");
+   {
+  
+      if(keyIsDown(UP_ARROW))
+  
+      background("HOTPINK");
+                                                              
+      if(keyIsDown(LEFT_ARROW))
+  
+      background("CYAN");
+  
+      if(keyIsDown(RIGHT_ARROW))
+  
+      background("YELLOW");
+   }
+   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
